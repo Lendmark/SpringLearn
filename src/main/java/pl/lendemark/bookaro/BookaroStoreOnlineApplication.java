eb3fcff316f2645ddad8efd65ed3fb98b177ed07
Lendemark <piotrek19933@gmail.com>
2021-11-20 23:23:53 +0100

    M2L4 - Dodanie pierwszego beana Springowego

---
+++ src/main/java/pl/lendemark/bookaro/BookaroStoreOnlineApplication.java
@@ -0,0 +1,31 @@
+package pl.lendemark.bookaro;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+
+@SpringBootApplication
+public class BookaroStoreOnlineApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookaroStoreOnlineApplication.class, args);
+
+    }
+
+    private final CatalogService catalogService;
+
+
+    public BookaroStoreOnlineApplication(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //CatalogService service = new CatalogService();
+        List<Book> books = catalogService.findByTitle("Pan Tadeusz");
+        books.forEach(System.out::println);
+    }
+}
---
+++ src/test/java/pl/lendemark/bookaro/BookaroStoreOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lendemark.bookaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaroStoreOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
